08a51e2955f96334726e14b9bc5d6d3e0a7a0e62
jjomarron <80477575+jjomarron@users.noreply.github.com>
2021-04-01 15:49:12 -0500

    Update EPL Initial Check.py

--- EPL Initial Check.py
+++ EPL Initial Check.py
@@ -33,33 +33,31 @@ profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 browser = webdriver.Firefox(profile)
 browser.implicitly_wait(10)
 
-df = pd.read_csv(r"C:\Users\Jack\Documents\Misc\Friend Help\Fantasy Soccer\stest.csv",encoding='UTF-8')
+df = pd.read_csv(r"C:\Users\Jack\Documents\Misc\Friend Help\Fantasy Soccer\test.csv",encoding='UTF-8')
 
 df['ID'] = pd.Series(df['ID'], dtype="string")
 
-my_cols = [str(i) for i in range(25)]
+my_cols = [str(i) for i in range(11)]
 df1 = pd.DataFrame()
 
 
 for i in range(len(df)):
     browser.get('https://www.whoscored.com/Players/'+df['ID'][i])
-    browser.find_element_by_css_selector("#sub-navigation > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)").click()
+    browser.find_element_by_css_selector(" #sub-navigation > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)").click()
     #table = browser.find_elements_by_css_selector("#top-player-stats-summary-grid")
-    row = browser.find_elements_by_xpath("//table[@class='grid with-centered-columns hover']/tbody/tr[1]")  
+    row = browser.find_elements_by_xpath("//table[@class='grid with-centered-columns hover']/tbody/tr[1]")
     for r in row:
         print(r.text)
-        list = [i]
-        hold = pd.read_csv(StringIO(r.text),sep=' ',names=my_cols,header=None,index_col=list,lineterminator='\t')
+        hold = pd.read_csv(StringIO(r.text),sep=' ',names=my_cols,header=None,lineterminator='\t')
         df1 = df1.append(hold)
-        browser.find_element_by_css_selector("#player-matches-stats-options > li:nth-child(2) > a:nth-child(1)").click()
-    
+
 df1=df1.reset_index()
-df1=df1.drop(['level_0','level_1','level_2'],axis=1)
+df1=df1.drop(['level_0','level_1','level_2','0','1'],axis=1)
 df1=df1.rename(columns={"2": "Minutes", "3": "Goals", "4": "Assists", "5": "Yellow", "6": "Red", "7": "Shots", "8": "PS%", "9": "Aerials Won", "10": "Rating"})
 
 result = pd.concat([df, df1], axis=1, join="inner")
 
-result.to_csv('initial_stat_check.csv', mode='w',index=False)
+result.to_csv('initial_check.csv', mode='a',index=False)
 
 
 
